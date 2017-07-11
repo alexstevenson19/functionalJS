028ea0f3acea765fe252eea91034c2e13e915321
Alex Stevenson <alexstevenson19@gmail.com>
2017-07-10 22:01:49 -0500

    make closure function to change first names, also split the first questions into separate areas so the functions wouldn't conflict

--- closures/closures_exercises.js
+++ closures/closures_exercises.js
@@ -1,8 +1,40 @@
-
+//1,2
 var nonsense = function(string){
-
 	var blab = function(){
 		alert(string);
 	};
-	setTimeOut(blab, 2000);
+
+	setTimeout(blab, 2000);
 };
+
+nonsense('no sense?');
+
+
+// 3, 4 =============================================
+
+var nonsense = function(string){
+	var blab = function(){
+		alert(string);
+	};
+	return blab;
+};
+
+var blabLater = nonsense('test1');
+var blabAgainLater = nonsense('test2');
+
+blabLater();
+blabAgainLater();
+
+// 5 =============================================
+
+var lastNameTrier = function(lastName){
+
+	var innerFunction = function(firstName){
+		console.log(firstName + ' ' + lastName);
+	}
+	return innerFunction;
+}
+
+var firstNameFarmer = lastNameTrier('Stevenson');
+firstNameFarmer('Alex');
+

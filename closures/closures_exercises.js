dc6d58e79f1fee01b9849aa08f755ff4c72f6875
Alex Stevenson <alexstevenson19@gmail.com>
2017-07-11 22:34:21 -0500

    use the module pattern to make a toaster. User has access to the timer the toaster is set at, but does not have access to the toastStatus.

--- closures/closures_exercises.js
+++ closures/closures_exercises.js
@@ -1,3 +1,5 @@
+// Run in console due to the alerts in the first couple functions.
+
 //1,2
 var nonsense = function(string){
 	var blab = function(){
@@ -25,26 +27,28 @@ var blabAgainLater = nonsense('test2');
 blabLater();
 blabAgainLater();
 
-// 5 =============================================
+// 5 a function with a closure =============================================
 
-var lastNameTrier = function(lastName){
+var lastNameTrier = function(firstName){
 
-	var innerFunction = function(firstName){
+	var innerFunction = function(lastName){
 		console.log(firstName + ' ' + lastName);
 	}
 	return innerFunction;
 }
 
-var firstNameFarmer = lastNameTrier('Stevenson');
-firstNameFarmer('Alex');
+var firstNameAlex = lastNameTrier('Alex');
+firstNameAlex('Kitabatake');
 
-// 6 ============================================
+// 6 function that returns an object with two methods. ============================================
 
 var storyWriter = function(){
 	var story = '';
 	return {
 		addWords: function(sentence){
 			return story = story + " " + sentence;
+			// story += sentence + '';
+			// return story.trim();
 		},
 		erase: function(){
 			return story = "";
@@ -54,4 +58,46 @@ var storyWriter = function(){
 
 // var farmLoveStory = storyWriter();
 // farmLoveStory.addWords('There was once a lonely cow.');
+
+// 7 module pattern ============================================
+
+var Toaster = function(){
+	var toastStatus = '';
+
+	var dry = function(){
+		toastStatus = 'not toasted'
+	};
+
+	var brown = function(){
+		toastStatus = 'just right';
+		// return toastStatus;
+	};
+	var burn = function(){
+		toastStatus = 'burned toast';
+		// return toastStatus;		
+	};
+
+	return {
+		toastTimer: function(time){
+			if (time <= 2){
+				dry();
+				return toastStatus;
+			}
+			else if (time > 2 && time < 5){
+				brown();
+				return toastStatus;
+			}
+			else {
+				burn();
+				return toastStatus;
+			}
+		}
+	};
+}
+
+
+
+
+
+
 
